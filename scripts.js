de3a37311aaf613bf7603c7cc80ca9d1ad81ef35
Maicolnine <maicolstwarmurillo@gmail.com>
2024-11-21 11:34:06 -0500

    cambios resubidos.

--- scripts.js
+++ scripts.js
@@ -249,3 +249,16 @@ function mostrarSeccion(seccionId) {
         seccionActiva.classList.add('visible');
     }
 }
+
+function mostrarSeccion(seccionId) {
+
+    const secciones = document.querySelectorAll('main > section');
+    secciones.forEach(seccion => {
+        seccion.classList.remove('visible');
+        seccion.classList.add('hidden');
+    });
+
+    const seccionActiva = document.getElementById(seccionId);
+    seccionActiva.classList.remove('hidden');
+    seccionActiva.classList.add('visible');
+}
